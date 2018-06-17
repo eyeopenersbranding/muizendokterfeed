ce55cdc9b6a785dbc7596831756521d03837bf12
eyeopenersbranding <c.keijzers@eyeopenersbranding.nl>
2018-06-17 16:06:29 +0200

    Het algemene logboek toegevoegd in het menu

--- page_includes/aside.php
+++ page_includes/aside.php
@@ -12,12 +12,12 @@
             </div>
 
             <div id="main-menu" class="main-menu collapse navbar-collapse">
-				
-				
+
+
                 <ul class="nav navbar-nav">
 					 <h3 class="menu-title"><?php echo $_SESSION['usr_name'];  ?> <?php echo $_SESSION['usr_surname'];  ?></h3><!-- /.menu-title -->
-					
-				
+
+
 					<?php if ($user_role == $Admin) { ?>
 					<li>
                         <a href="ad-log-results.php"> <i class="menu-icon fa fa-th"></i>KPI's Nederland</a>
@@ -28,22 +28,26 @@
 					<li>
                         <a href="ad-log-results-ho.php"> <i class="menu-icon fa fa-th"></i>KPI's Houtworm</a>
                     </li>
-				
+
 					<?php } if ($user_role == $Planning) { ?>
-				
+
 					<li>
                         <a href="log.php?name=&tel="> <i class="menu-icon fa fa-th"></i>Logboek</a>
                     </li>
 					<li>
                         <a href="logs.php"> <i class="menu-icon fa fa-search"></i>Logboek doorzoeken </a>
                     </li>
+
+                    <li>
+                                  <a href="alllogs.php"> <i class="menu-icon fa fa-search"></i>Algemeen logboek</a>
+                              </li>
 					<li>
                         <a href="important-data.php"> <i class="menu-icon fa fa-search"></i>Belangrijke gegevens</a>
                     </li>
 
 					<?php }?>
-					
-            
+
+
                 </ul>
             </div><!-- /.navbar-collapse -->
         </nav>
