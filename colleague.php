4ca2bf68953575ba6f9fe2cd7fe35c0e99b3e462
eyeopenersbranding <c.keijzers@eyeopenersbranding.nl>
2018-06-17 16:28:53 +0200

    Accountnaam pushing aangepast van nummeriek naar alfabetisch

--- colleague.php
+++ colleague.php
@@ -6,7 +6,7 @@ require_once 'page_includes/data_config.php';
 
 
 if (isset($_SESSION['usr_id'])) {
-    
+
 } else {
     header('Location: login.php');
 
@@ -25,73 +25,73 @@ include 'page_includes/ikwil_redirect_core.php';
 	}
 
 
-
+  $accountname = $_SESSION['usr_name'];
 	//======================================================================
 	// Engine voor het updaten van de profiel gegevens
 	//======================================================================
 
     //Script activeren wanneer de submit knop wordt geactiveerd
     if(isset($_POST['submit_new_prospect'])&& (!empty($_POST)) && $_SERVER['REQUEST_METHOD'] == 'POST'){
-		
+
 		$error = false;
-		
+
 		//Alle variable opsommen en ontrekken van het formulier
 		$log_name = $_POST['log_name'];
 		$log_notes = $_POST['log_notes'];
 
-		
+
 		//Check voor de klantnaam
 		if (empty($log_name)) {
 		$error = true;
 		$log_name_error = ($error_notification['log_name_error']);
 		$error_message = ($error_notification['log_name_error']);
 		}
-		
-	
-		
+
+
+
 		//Als er geen error is ga dan door met het updaten van de gegevens
 		if (!$error){
-			
-			date_default_timezone_set('Europe/Amsterdam');	
+
+			date_default_timezone_set('Europe/Amsterdam');
 			setlocale(LC_ALL, 'nl_NL');
-			
-			
+
+
 			$log_date = date('Y-m-d h:i:s a', time());
-			
+
 			$log_colleague = "1";
-			
+
 			$sql = "INSERT INTO md_log
 
 			(
 			log_date,
-			log_name, 
+			log_name,
 			log_notes,
 			log_colleague,
 			log_user_id,
 			log_land
-			) 
+			)
 			VALUES (
 			'$log_date',
-			'$log_name', 
+			'$log_name',
 			'$log_notes',
 			'$log_colleague',
-			'$user_id',
+			'$accountname',
 			'$land'
-			)";	
+			)";
 
 			$result = mysqli_query($con, $sql);
 
-			if ($result === TRUE) 
-			{	
+			if ($result === TRUE)
+			{
 				//Succes melding printen
 				$log_success = ($success_notification['log_success']);
 				$success_message = ($success_notification['log_success']);
-				
+
 				//Ververs pagina om de foutmelding te dumpen
 				header("Refresh:1; url=log.php");
-			} 
-			else 
-				
+			}
+			else
+
 			{
 				//Wanneer de data niet verwerkt kan worden in de database printen we een fatale fout
     			echo "Er is een ernstige fout opgetreden";
@@ -103,12 +103,12 @@ include 'page_includes/ikwil_redirect_core.php';
 <?php include 'page_includes/header.php';?>
 
 <body>
-	
-	
+
+
 	<?php include 'page_includes/aside.php';?>
         <!-- Left Panel -->
 
-    
+
 
 
     <!-- Right Panel -->
@@ -140,13 +140,13 @@ include 'page_includes/ikwil_redirect_core.php';
 
         <div class="content mt-3">
             <div class="animated fadeIn">
-			
+
 					<!-- Alle foutmeldingen worden geprint in de alert box -->
 					<?php if (isset($error_message)) { ?>
 						<div class="alert alert-danger">
 						 <button type="button" aria-hidden="true" class="close"><i class="material-icons">close</i></button>
 							 <span>
-								<?php if (isset($log_name_error)) echo $log_name_error; ?> 
+								<?php if (isset($log_name_error)) echo $log_name_error; ?>
 							</span></div>
 					<?php } ?>
 
@@ -155,17 +155,17 @@ include 'page_includes/ikwil_redirect_core.php';
 						<div class="alert alert-success">
 						 <button type="button" aria-hidden="true" class="close"><i class="material-icons">close</i></button>
 							 <span>
-								<?php if (isset($log_success)) echo $log_success; ?> 
+								<?php if (isset($log_success)) echo $log_success; ?>
 							</span></div>
 					<?php } ?>
-               
-                    
 
-            
+
+
+
 
                     <div class="col-sm-12 mb-4">
-                
-						
+
+
 						      <div class="col-lg-6">
 								<div class="card">
 								<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
@@ -174,28 +174,28 @@ include 'page_includes/ikwil_redirect_core.php';
 									<div class="form-group"><label for="company" class=" form-control-label">Naam</label>
 									<input type="text" name="log_name" id="company" placeholder="Naam collega" class="form-control">
 									</div>
-					
+
 									 <div class="row form-group">
 										<div class="col col-md-3"><label for="textarea-input" class=" form-control-label">Reden</label></div>
 										<div class="col-12 col-md-9"><textarea name="log_notes" id="textarea-input" rows="9" placeholder="Typ hier.." class="form-control"></textarea></div>
 									  </div>
-									  
-									
+
+
 									  <button type="submit" name="submit_new_prospect" class="btn btn-outline-success">Verwerken</button>
-				
+
 								  </div>
 								</form>
 								</div>
 							  </div>
 
-					
+
 
                     </div>
 
 
-               
 
-                  
+
+
 
 
 
